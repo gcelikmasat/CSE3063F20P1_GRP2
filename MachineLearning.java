6523ba975704d67080edb94659a989a67622a9ed
kadiracunn <74609387+kadiracunn@users.noreply.github.com>
2020-11-30 16:40:21 +0300

    Add files via upload

---
+++ MachineLearning.java
@@ -0,0 +1,15 @@
+package CSE3063F20P1_GRP2;
+
+import java.util.ArrayList;
+
+public class MachineLearning extends User {
+
+	public MachineLearning(int id, String name, String user_type) {
+		super(id, name, user_type);
+	}
+
+	void label(LabelAssignments la, ArrayList<Label> l,int max) {
+		System.out.println("Machine Learning Bot.");
+
+	}
+}
